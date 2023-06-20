d5404eba87dd16be9cdc205505f1d16fc2594a95
Rob Godfrey <rob_godfrey@outlook.com>
2023-06-19 18:34:01 -1000

    Update code for Jetson

--- camera.cpp
+++ camera.cpp
@@ -1,24 +1,32 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <stdio.h>
 
 using namespace cv;
+using namespace std;
 
-int main() {
-    std::cout << "START" << std::endl;
+int main(int, char**) {
+    Mat frame;
+    VideoCapture cap("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=960, height=616, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True");
     
-    Mat image;
-    namedWindow("Display window");
-    VideoCapture cap(0);
-
     if (!cap.isOpened()) {
-        std::cout << "Can't open camera :(" << std::endl;
-        exit(1);
+        cerr << "ERROR! Unable to open camera" << endl;
+        return -1;
     }
-
+    cout << "Starting camera" << endl
+         << "Press any key to terminate" << endl;
     while (true) {
-        cap >> image;
-        imshow("Display window", image);
-        waitKey(25);
+        cap.read(frame);
+        if (frame.empty()) {
+            cerr << "ERROR! blank frame grabbed" << endl;
+            break;
+        }
+        imshow("Live", frame);
+        if (waitKey(5) >= 0)
+            break;
     }
-
+    cout << "Stopping" << endl;
     return 0;
 }
